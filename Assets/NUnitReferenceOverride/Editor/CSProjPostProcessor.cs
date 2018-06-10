166f3a995f5eda7a5a69d9ce95c6a7ca5e5003b4
FrankNine <aaefiikmnnnr@gmail.com>
2018-06-10 16:25:22 +0800

    feat: Allow user to set custom .csproj filename
    Visual Studio Tools for Unity allows users to change .csproj filename on generation:
    https://docs.microsoft.com/en-us/visualstudio/cross-platform/customize-project-files-created-by-vstu
    Add new options to support that

--- Assets/NUnitReferenceOverride/Editor/CSProjPostProcessor.cs
+++ Assets/NUnitReferenceOverride/Editor/CSProjPostProcessor.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Xml.Linq;
 
@@ -12,13 +13,13 @@ public class SolutionPostProcessor : AssetPostprocessor
 	
 	private static void OnGeneratedCSProjectFiles()
 	{
-#if UNITY_5_6_OR_NEWER
+#if UNITY_5_6_OR_NEWER && UNITY_EDITOR_WIN
 		if (!NUnitPreferenceItem.IsOverridingNUnitReference) { return; }
-		_OverrideNUnitReference();
+	    OverrideNUnitReference();
 #endif
-	}
+    }
 
-	private static void _OverrideNUnitReference()
+    public static void OverrideNUnitReference()
 	{
 		Debug.Log("Patching .csproj for overriding NUnit reference...");
 		
@@ -26,8 +27,11 @@ public class SolutionPostProcessor : AssetPostprocessor
 		
 		string projectRootPath = Directory.GetParent(Application.dataPath).FullName;
 		string editorCSProjPath = _GetEditorCSProjPath(projectRootPath);
+        if (string.IsNullOrEmpty(editorCSProjPath)) { return; }
 
-		var csprojDocument = XDocument.Load(editorCSProjPath);
+        Debug.LogFormat("Patching {0}...", editorCSProjPath);
+
+        var csprojDocument = XDocument.Load(editorCSProjPath);
 		var referenceNodes = csprojDocument.Root.Descendants(ns + "Reference").ToArray();
 		var nunitReferenceNodeArray = referenceNodes.Where(_IsNUnitReference).ToArray();
 		if (nunitReferenceNodeArray.Length == 0)
@@ -45,18 +49,60 @@ public class SolutionPostProcessor : AssetPostprocessor
 
     private static string _GetEditorCSProjPath(string projectRootPath)
     {
-#if UNITY_EDITOR_WIN
+        if (NUnitPreferenceItem.IsUsingCustomEditorCSProjFilename)
+        {
+            string customEditorCSProjPath = Path.Combine(projectRootPath, NUnitPreferenceItem.CustomCSProjFilename);
+            if (File.Exists(customEditorCSProjPath))
+            {
+                return customEditorCSProjPath;
+            }
+            else
+            {
+                Debug.LogWarningFormat("NUnit override cannot find custom project file at: {0}",
+                    customEditorCSProjPath);
+            }
+        }
+
+        {
+            string vstuEditorCSProjPath = _GetVSTUEditorCSProjPath(projectRootPath);
+            if (File.Exists(vstuEditorCSProjPath))
+            {
+                return vstuEditorCSProjPath;
+            }
+            else
+            {
+                Debug.LogWarningFormat(
+                    "NUnit override cannot find VSTU patched project file at: {0}, did you install Visual Studio Tools for Unity?",
+                    vstuEditorCSProjPath);
+            }
+        }
+
+        {
+            string defaultEditorCSProjPath = Path.Combine(projectRootPath, CSPROJ_DEFAULT_FILENAME);
+            if (File.Exists(defaultEditorCSProjPath))
+            {
+                return defaultEditorCSProjPath;
+            }
+            else
+            {
+                Debug.LogWarningFormat("NUnit override cannot find project file at: {0}. Give up patching",
+                    defaultEditorCSProjPath);
+                return string.Empty;
+            }
+        }
+    }
+
+    private static string _GetVSTUEditorCSProjPath(string projectRootPath)
+    {
         string projectName = Path.GetFileName(projectRootPath);
         string editorCSProjFilename = string.Format("{0}.Editor.csproj", projectName);
         return Path.Combine(projectRootPath, editorCSProjFilename);
-#else
-        return Path.Combine(projectRootPath, CSPROJ_DEFAULT_FILENAME);
-#endif
     }
 
     private static bool _IsNUnitReference(XElement element)
 	{
 		XAttribute includeAttribute = element.Attribute("Include");
-		return includeAttribute != null && includeAttribute.Value == "nunit.framework";
+		return includeAttribute != null && 
+		       string.Equals(includeAttribute.Value , "nunit.framework", StringComparison.OrdinalIgnoreCase);
 	}
 }
--- Assets/NUnitReferenceOverride/Editor/NUnitPreferenceItem.cs
+++ Assets/NUnitReferenceOverride/Editor/NUnitPreferenceItem.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 using UnityEngine;
 using UnityEditor;
 
@@ -6,57 +7,103 @@ public class NUnitPreferenceItem {
 
 	private static bool _isPrefsLoaded;
 
-	private const string IsOverridingNUnitReferenceEditorPrefsKey = "IsOverridingNUnitReference";
-	private const string OverridingNUnitPathEditorPrefsKey = "OverridingNUnitPath";
-	
-	private static bool _isOverridingNUnitReference;
-	private static string _overridingNUnitPath;
-	
-	public static bool IsOverridingNUnitReference
-	{
-		get { return EditorPrefs.GetBool(IsOverridingNUnitReferenceEditorPrefsKey, false); }
-	}
+    private const string IsOverridingNUnitReferenceEditorPrefsKey  = "IsOverridingNUnitReference";
+    private const string OverridingNUnitPathEditorPrefsKey         = "OverridingNUnitPath";
+    private const string IsUsingCustomEditorCSProjFilenamePrefsKey = "IsUsingCustomEditorCSProjFilename";
+    private const string CustomEditorCSProjFilenamePrefsKey        = "CustomEditorCSProjFilename";
 
-	public static string OverridingNUnitPath
-	{
-		get{return EditorPrefs.GetString(OverridingNUnitPathEditorPrefsKey, String.Empty);}
-	}
-	
-	[PreferenceItem("NUnit Override")]
+    private static bool   _isOverridingNUnitReference;
+    private static string _overridingNUnitPath;
+    private static bool   _isUsingCustomEditorCSProjFilename;
+    private static string _customCSProjName;
+
+    public static bool IsOverridingNUnitReference
+    {
+        get { return EditorPrefs.GetBool(IsOverridingNUnitReferenceEditorPrefsKey, false); }
+    }
+
+    public static string OverridingNUnitPath
+    {
+        get { return EditorPrefs.GetString(OverridingNUnitPathEditorPrefsKey, String.Empty); }
+    }
+
+    public static bool IsUsingCustomEditorCSProjFilename
+    {
+        get { return EditorPrefs.GetBool(IsUsingCustomEditorCSProjFilenamePrefsKey, false); }
+    }
+
+    public static string CustomCSProjFilename
+    {
+        get { return EditorPrefs.GetString(CustomEditorCSProjFilenamePrefsKey, String.Empty); }
+    }
+
+    [PreferenceItem("NUnit Override")]
 	private static void PreferencesGUI()
 	{
-#if UNITY_5_6_OR_NEWER
-		_ShowNUnitOverrideOptions();
+#if UNITY_EDITOR_OSX
+        _ShowOverrideNotRequiredRiderOSX();
+#elif !UNITY_5_6_OR_NEWER
+        _ShowOverrideNotRequiredNUnit2();
 #else
-		_ShowOverrideNotRequried();
+        _ShowNUnitOverrideOptions();
 #endif
-	}
+    }
 
 	private static void _ShowNUnitOverrideOptions()
 	{
 		if (!_isPrefsLoaded)
 		{
-			_isOverridingNUnitReference = IsOverridingNUnitReference;
-			_overridingNUnitPath = OverridingNUnitPath;
-			
-			_isPrefsLoaded = true;
+		    _isOverridingNUnitReference = IsOverridingNUnitReference;
+		    _overridingNUnitPath = OverridingNUnitPath;
+		    _isUsingCustomEditorCSProjFilename = IsUsingCustomEditorCSProjFilename;
+		    _customCSProjName = CustomCSProjFilename;
+
+            _isPrefsLoaded = true;
 		}
 
-		_isOverridingNUnitReference = EditorGUILayout.Toggle("Is Overriding .csproj NUnit reference", 
+		_isOverridingNUnitReference = EditorGUILayout.Toggle("Is Overriding NUnit reference", 
 			_isOverridingNUnitReference);
+
 		GUI.enabled = _isOverridingNUnitReference;
-		_overridingNUnitPath = EditorGUILayout.TextField(_overridingNUnitPath);
-		GUI.enabled = true;
+	    {
+	        EditorGUILayout.LabelField("nunit.framework.dll path (NUnit 3.5, targeting .Net 3.5)");
+	        _overridingNUnitPath = EditorGUILayout.TextField(_overridingNUnitPath);
+
+	        _isUsingCustomEditorCSProjFilename = EditorGUILayout.Toggle("Is using custom .csproj filename",
+	            _isUsingCustomEditorCSProjFilename);
+
+	        GUI.enabled = _isOverridingNUnitReference && _isUsingCustomEditorCSProjFilename;
+	        {
+	            EditorGUILayout.LabelField("Custom editor .csproj name (With extension)");
+	            _customCSProjName = EditorGUILayout.TextField(_customCSProjName);
+	        }
+	        GUI.enabled = _isOverridingNUnitReference;
+        }
+
+	    if (GUILayout.Button("Patch now"))
+	    {
+	        SolutionPostProcessor.OverrideNUnitReference();
+        }
+        GUI.enabled = true;
 
 		if (GUI.changed)
 		{
 			EditorPrefs.SetBool(IsOverridingNUnitReferenceEditorPrefsKey, _isOverridingNUnitReference);
 			EditorPrefs.SetString(OverridingNUnitPathEditorPrefsKey, _overridingNUnitPath);
+		    EditorPrefs.SetBool(IsUsingCustomEditorCSProjFilenamePrefsKey, _isUsingCustomEditorCSProjFilename);
+            EditorPrefs.SetString(CustomEditorCSProjFilenamePrefsKey, _customCSProjName);
 		}
 	}
 
-	private static void _ShowOverrideNotRequried()
+	private static void _ShowOverrideNotRequiredNUnit2()
 	{
-		EditorGUILayout.LabelField("This version of Unity is using NUnit 2. Overriding is not required.");
+		EditorGUILayout.LabelField("This version of Unity is using NUnit 2.");
+	    EditorGUILayout.LabelField("Overriding is not required.");
 	}
+
+    private static void _ShowOverrideNotRequiredRiderOSX()
+    {
+        EditorGUILayout.LabelField("This plugin is only for Visual Studio");
+        EditorGUILayout.LabelField("for Windows with ReSharper");
+    }
 }
